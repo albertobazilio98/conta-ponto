02ca232ae776a4982da6f54e2f2eae9f8b67d7c0
Beto <albertobazilio@jera.com.br>
2022-01-07 09:05:12 -0400

    [NEW] time counter

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,50 @@
-<script lang="ts">
-	export let name: string;
-</script>
-
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div class="continer">
+    <MaskInput alwaysShowMask class="time-input" maskChar="_" mask="00:00" on:change={(value) => handleChange(value, 0)} />
+    <MaskInput alwaysShowMask class="time-input" maskChar="_" mask="00:00" on:change={(value) => handleChange(value, 1)} />
+    <MaskInput alwaysShowMask class="time-input" maskChar="_" mask="00:00" on:change={(value) => handleChange(value, 2)} />
+    <MaskInput alwaysShowMask class="time-input" maskChar="_" mask="00:00" on:change={(value) => handleChange(value, 3)} />
+    {time()}
+  </div>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<script lang="ts">
+  import MaskInput from "svelte-input-mask/MaskInput.svelte";
+  import { parse, isValid, getUnixTime, fromUnixTime, format, addSeconds } from 'date-fns'
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  const handleChange = ({ detail }, index) => {
+    times[index] = detail.inputState.maskedValue
+  };
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  let times = [
+    '',
+    '',
+    '',
+    '',
+  ]
+
+  $: time = () => {
+    const dates = times.map((time) => parse(time, 'HH:mm', new Date()))
+    if (dates.every((date) => isValid(date))) {
+      const timestamps = dates.map((date) => getUnixTime(date))
+      const difference = - timestamps[0] + timestamps[1] + (- timestamps[2] + timestamps[3])
+      return format(addSeconds(new Date(2022, 0, 6), difference), 'HH:mm')
+    }
+  }
+</script>
+
+
+<style lang="scss">
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
