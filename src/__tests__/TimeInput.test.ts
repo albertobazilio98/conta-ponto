74a1d417e84e8f96f44ba242d9af3c4f28beaacc
Beto <albertobazilio98@gmail.com>
2022-01-25 18:27:42 -0400

    [UPDATE] invalid time test case

--- src/__tests__/TimeInput.test.ts
+++ src/__tests__/TimeInput.test.ts
@@ -1,9 +1,10 @@
-import { render, screen, fireEvent, prettyDOM } from '@testing-library/svelte'
+import { render, screen, fireEvent } from '@testing-library/svelte'
+import { parse, isValid, getUnixTime } from 'date-fns'
 
 import TimeInput from '../components/TimeInput.svelte'
 
-describe('test input tests', () => {
-  test('should convert valid time input to date', async () => {
+describe('time input timeChange triggering', () => {
+  test('should return valid date on valid input', async () => {
     const props = {
       label: 'testing',
       id: 'testing-id',
@@ -13,9 +14,10 @@ describe('test input tests', () => {
     const input = screen.queryByDisplayValue('__:__')
     const inputedTime = '08:00'
     const mockEvent = jest.fn()
+    let data;
     component.$on('timeChange', (e) => {
-      console.log('happened')
-      mockEvent(e.detail)
+      mockEvent(e.detail);
+      data = e.detail;
     })
     await fireEvent.focus(input)
     await fireEvent.select(input, {
@@ -29,8 +31,44 @@ describe('test input tests', () => {
         getData: () => inputedTime
       }
     })
-    
-    console.log(mockEvent.mock.calls)
-    expect(mockEvent).toHaveBeenCalled();
+
+    expect(mockEvent).toBeCalled();
+    expect(data.date).toEqual(new Date(2022, 0, 6, 8, 0));
+    expect(data.value).toEqual(inputedTime);
+    expect(data.seconds).toEqual(8 * 60 * 60);
+  })
+
+  test('should not return date on invalid input', async () => {
+    const props = {
+      label: 'testing',
+      id: 'testing-id',
+      value: '',
+    }
+    const { component } = render(TimeInput, { props })
+    const input = screen.queryByDisplayValue('__:__')
+    const inputedTime = '25:00'
+    const mockEvent = jest.fn()
+    let data;
+    component.$on('timeChange', (e) => {
+      mockEvent(e.detail);
+      data = e.detail;
+    })
+    await fireEvent.focus(input)
+    await fireEvent.select(input, {
+      target: {
+        selectionStart: 0,
+        selectionEnd: 5,
+      }
+    })
+    await fireEvent.paste(input, {
+      clipboardData: {
+        getData: () => inputedTime
+      }
+    })
+
+    expect(mockEvent).toBeCalled();
+    expect(data.date).toBeFalsy();
+    expect(data.value).toEqual(inputedTime);
+    expect(data.seconds).toBeFalsy();
   })
 })
